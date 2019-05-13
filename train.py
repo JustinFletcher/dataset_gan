65dc469ca0d1708bfcb61b9b56cc6348392fb4c3
Justin Fletcher <justinr.fletcher@gmail.com>
2019-05-13 12:02:00 -1000

    Added CLI for GPU.

--- train.py
+++ train.py
@@ -26,20 +26,17 @@ import os
 import time
 import argparse
 import tensorflow as tf
-
-# import data_provider
 import networks
 import util
 
+
+# Import contrib GAN.
 from tensorflow.contrib.gan.python import losses as tfgan_losses
 
-
+# Import DatasetGenerator abstraction.
 from dataset.dataset_generator import DatasetGenerator
 
 tfgan = tf.contrib.gan
-
-# tf.enable_eager_execution()
-
 
 def _learning_rate(gan_type):
     # First is generator learning rate, second is discriminator learning rate.
@@ -50,13 +47,15 @@ def _learning_rate(gan_type):
     }[gan_type]
 
 # Two core changes in Keras
-# Remove buggy error checks.FLAGS
+# Remove buggy error checks.
 def main(FLAGS):
 
+
+    # Create the log directory, if it doesn't exist.
     if not tf.gfile.Exists(FLAGS.train_log_dir):
         tf.gfile.MakeDirs(FLAGS.train_log_dir)
 
-    # Set the GPUs we want the script to use/see
+    # Restrict GPU usage to avoid stepping on other work.
     print("GPU List = " + str(flags.gpu_list))
     os.environ["CUDA_VISIBLE_DEVICES"] = flags.gpu_list
 
@@ -68,11 +67,13 @@ def main(FLAGS):
             train_tfrecord_name = os.path.join(FLAGS.dataset_path,
                                                FLAGS.train_tfrecord)
 
+
+            # Declare a helper function to cohere w/ the Generator.
             def cast_image_to_float(image, bbox=[]):
 
                 return tf.cast(image, tf.float32)
 
-            # Instantiate a wrapper .
+            # Instantate a helper class, returns a standard TF DS Generator.
             train_generator = DatasetGenerator(train_tfrecord_name,
                                                num_channels=FLAGS.num_channels,
                                                augment=FLAGS.augment_train_data,
@@ -82,61 +83,70 @@ def main(FLAGS):
                                                buffer=FLAGS.dataset_buffer_size,
                                                encoding_function=cast_image_to_float)
 
+            # Build the iterator from the geenerator
             train_iterator = train_generator.dataset.make_one_shot_iterator()
 
+            # Get a prototpye batch for pipeline construction.
             images = train_iterator.get_next()
 
             print("\n\n\n\n\n")
             print(tf.shape(images))
             print("\n\n\n\n\n")
-            # images, one_hot_labels, _ = data_provider.provide_data(
-            #     'train', FLAGS.batch_size, FLAGS.dataset_dir, num_threads=4)
+
+    # Set the mutual information weight penalty to 0, overriden if infogan.
+    mutual_information_penalty_weight = 0.0
 
     # Define the GANModel tuple. Optionally, condition the GAN on the label or
     # use an InfoGAN to learn a latent representation.
     if FLAGS.gan_type == 'unconditional':
-        gan_model = tfgan.gan_model(
-            generator_fn=networks.unconditional_generator,
-            discriminator_fn=networks.unconditional_discriminator,
-            real_data=images,
-            generator_inputs=tf.random_normal([FLAGS.batch_size,
-                                               FLAGS.noise_dims]))
+        gan_model = tfgan.gan_model(generator_fn=networks.unconditional_generator,
+                                    discriminator_fn=networks.unconditional_discriminator,
+                                    real_data=images,
+                                    generator_inputs=tf.random_normal([FLAGS.batch_size,
+                                                                       FLAGS.noise_dims]))
 
+    # TODO: Deprecate or extend conditional GAN.
     elif FLAGS.gan_type == 'conditional':
         noise = tf.random_normal([FLAGS.batch_size, FLAGS.noise_dims])
-        gan_model = tfgan.gan_model(
-            generator_fn=networks.conditional_generator,
-            discriminator_fn=networks.conditional_discriminator,
-            real_data=images,
-            generator_inputs=(noise, one_hot_labels))
+        gan_model = tfgan.gan_model(generator_fn=networks.conditional_generator,
+                                    discriminator_fn=networks.conditional_discriminator,
+                                    real_data=images,
+                                    generator_inputs=(noise, one_hot_labels))
 
     elif FLAGS.gan_type == 'infogan':
         cat_dim, cont_dim = 10, 2
-        generator_fn = functools.partial(
-            networks.infogan_generator, categorical_dim=cat_dim)
-        discriminator_fn = functools.partial(
-            networks.infogan_discriminator, categorical_dim=cat_dim,
-            continuous_dim=cont_dim)
+        generator_fn = functools.partial(networks.infogan_generator,
+                                         categorical_dim=cat_dim)
+        discriminator_fn = functools.partial(networks.infogan_discriminator,
+                                             categorical_dim=cat_dim,
+                                             continuous_dim=cont_dim)
         unstructured_inputs, structured_inputs = util.get_infogan_noise(
-            FLAGS.batch_size, cat_dim, cont_dim, FLAGS.noise_dims)
-        gan_model = tfgan.infogan_model(
-            generator_fn=generator_fn,
-            discriminator_fn=discriminator_fn,
-            real_data=images,
-            unstructured_generator_inputs=unstructured_inputs,
-            structured_generator_inputs=structured_inputs)
+            FLAGS.batch_size,
+            cat_dim,
+            cont_dim,
+            FLAGS.noise_dims)
+        gan_model = tfgan.infogan_model(generator_fn=generator_fn,
+                                        discriminator_fn=discriminator_fn,
+                                        real_data=images,
+                                        unstructured_generator_inputs=unstructured_inputs,
+                                        structured_generator_inputs=structured_inputs)
+
+        mutual_information_penalty_weight = 1
 
     # tfgan.eval.add_gan_model_image_summaries(gan_model, FLAGS.grid_size)
 
+    # Write a summary that save the real images.
     tf.summary.image('real_images',
-                 images[:, :, :, :3],
-                 max_outputs=2)
+                     images[:, :, :, :3],
+                     max_outputs=2)
 
     # Get the GANLoss tuple. You can pass a custom function, use one of the
     # already-implemented losses from the losses library, or use the defaults.
     with tf.name_scope('loss'):
-        mutual_information_penalty_weight = (1.0 if FLAGS.gan_type == 'infogan'
-                                             else 0.0)
+
+        # mutual_information_penalty_weight = (1.0 if FLAGS.gan_type == 'infogan'
+        #                                      else 0.0)
+
         gan_loss = tfgan.gan_loss(
             gan_model,
             generator_loss_fn=tfgan_losses.minimax_generator_loss,
@@ -231,6 +241,10 @@ if __name__ == '__main__':
                         type=int,
                         default=1000000,
                         help='The maximum number of gradient steps.')
+    
+    parser.add_argument('--gpu_list', type=str,
+                        default="0",
+                        help='GPUs to use with this model.')
 
     parser.add_argument('--gan_type',
                         type=str,
